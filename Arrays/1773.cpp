7b23dd175e648b222fd3956cc66f5b7f2c90ab08
Saurav Sutaria <73012044+Saurav-Sutaria@users.noreply.github.com>
2021-06-24 22:17:06 +0530

    Count Items Matching a Rule

---
+++ Arrays/1773.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int ind = 0;
+        if(ruleKey == "type"){
+            ind = 0;            
+        }else if(ruleKey == "color"){
+            ind = 1;
+        }else if(ruleKey == "name"){
+            ind = 2;
+        }
+        int ans = 0;
+        for(int i=0;i<items.size();i++){
+            if(items[i][ind] == ruleValue){
+                ans++;
+            }
+        }
+        return ans;
+        
+    }
+};
